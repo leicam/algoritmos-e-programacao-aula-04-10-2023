075b0128ec9b9e99fcea8cdd81761c7aa2ac77c6
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2023-10-04 20:25:31 -0300

    feat: Exemplos com laços de repetição while/do-while/for e implementação de uma calculadora com um menu gerenciado por um laço de repetição

---
+++ ExemplosLacosDeRepeticao/Calculadora.cs
@@ -0,0 +1,64 @@
+﻿namespace ExemplosLacosDeRepeticao
+{
+    /// <summary>
+    /// classe estática
+    /// </summary>
+    public static class Calculadora
+    {
+        /// <summary>
+        /// Solicita dois valores ao usuário e retorna o valor da soma
+        /// </summary>
+        public static void Soma() 
+        {
+            Console.WriteLine("Resultado Soma: "
+                + (ObterPrimeiroValor() + ObterSegundoValor()));
+        }
+
+        /// <summary>
+        /// Solicita dois valores ao usuário e retorna o valor da subtração
+        /// </summary>
+        public static void Subtracao() 
+        {
+            Console.WriteLine("Resultado Subtração: "
+                + (ObterPrimeiroValor() - ObterSegundoValor()));
+        }
+
+        /// <summary>
+        /// Solicita dois valores ao usuário e retorna o valor da divisão
+        /// </summary>
+        public static void Divisao() 
+        {
+            Console.WriteLine("Resultado Divisão: "
+                + (ObterPrimeiroValor() / ObterSegundoValor()));
+        }
+
+        /// <summary>
+        /// Solicita dois valores ao usuário e retorna o valor da multiplicação
+        /// </summary>
+        public static void Multiplicacao() 
+        {
+            Console.WriteLine("Resultado Multiplicação: "
+                + (ObterPrimeiroValor() * ObterSegundoValor()));
+        }
+
+        /// <summary>
+        /// Método estático com modificador de acesso privado
+        /// </summary>
+        /// <returns>decimal</returns>
+        private static decimal ObterPrimeiroValor()
+        {
+            Console.WriteLine("Informe o primeiro valor: ");
+            return decimal.Parse(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Método estático com modificador de acesso privado
+        /// </summary>
+        /// <returns>decimal</returns>
+        private static decimal ObterSegundoValor()
+        {
+            Console.WriteLine("Informe o segundo valor: ");
+            return decimal.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ ExemplosLacosDeRepeticao/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace ExemplosLacosDeRepeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("1");
+            //Console.WriteLine("2");
+            //Console.WriteLine("3");
+            //Console.WriteLine("10");
+
+            //Exemplo de laço de repetição while
+            //int contador = 10;
+            //while (contador  != -1)
+            //{
+            //    Console.WriteLine("WHILE: " + contador);
+            //    //contador = contador - 1;
+            //    contador--;
+            //}
+
+            //Exemplo de laço de repetição do/while
+            //int contadorDois = 10;
+            //do
+            //{
+            //    Console.WriteLine("DO/WHILE: " + contadorDois);
+            //    contadorDois--;
+            //} while (contadorDois >= 0);
+
+            //Exemplo de laço de repetição for
+            //for(int i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine("FOR: " + i);
+            //}
+
+            string opcao = "0";
+            do
+            {
+                try
+                {
+                    Console.WriteLine("**CALCULADORA**");
+                    Console.WriteLine("Selecione uma opção:");
+                    Console.WriteLine("1 - Soma");
+                    Console.WriteLine("2 - Subtração");
+                    Console.WriteLine("3 - Multiplicação");
+                    Console.WriteLine("4 - Divisão");
+                    Console.WriteLine("0 - Sair");
+
+                    opcao = Console.ReadLine();
+
+                    switch (opcao)
+                    {
+                        case "1":
+                            Calculadora.Soma();
+                            break;
+                        case "2":
+                            Calculadora.Subtracao();
+                            break;
+                        case "3":
+                            Calculadora.Multiplicacao();
+                            break;
+                        case "4":
+                            Calculadora.Divisao();
+                            break;
+                        default:
+                            Console.WriteLine("Opção inválida!");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } while (opcao != "0");
+
+            Console.WriteLine("Até logo");
+        }
+    }
+}
